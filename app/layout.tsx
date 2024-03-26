5a671e3c50c464c9d349e9941a28a376a4e7c0d6
Miguel Collaço <miguel.l.collaco@gmail.com>
2024-03-26 11:37:39 +0000

    Vercel Analytics +  Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
+import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,20 +14,22 @@ export const metadata: Metadata = {
   title: "Miguel Collaço",
   authors: { name: "Miguel Collaço" },
   description: "CSE student based in Portugal",
-  keywords: ["miguel", "collaco"],
+  keywords: ["miguel", "collaço"],
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${inter.className} scroll-smooth text-text overscroll-none bg-background`}>
-        <Nav/>
+        <Nav />
         {children}
-        <Footer/>
+        <Footer />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
